ef8a0d7407438682327ee2ccc5e1455e1de17ac3
Balogun Razak <razak2free@gmail.com>
2022-04-15 23:33:27 +0100

    15th April

---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from '../UI/Button/Button';
+import Card from '../UI/Card/Card';
+import classes from './Home.module.css';
+
+const Home = (props) => {
+  return (
+    <Card className={classes.home}>
+      <h1>Welcome back!</h1>
+      <Button onClick={props.onLogout}>Logout</Button>
+    </Card>
+  );
+};
+
+export default Home;
